a406afd1adf2d7851b24b7d0b511e9d42e57673b
MOONJUNSEO <answnstj1234@naver.com>
2025-11-04 10:27:57 +0900

    first

---
+++ src/main/java/com/back/ex1.java
@@ -0,0 +1,12 @@
+package com.back;
+
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) {
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명언) ");
+        Scanner scanner = new Scanner(System.in);
+        String cmd = scanner.nextLine();
+    }
+}
